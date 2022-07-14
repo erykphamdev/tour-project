be824af12043c73be7103fa53598486dc50f6fac
phamthang11782 <phamthang11782@gmail.com>
2022-07-15 06:11:59 +0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Fragment, useState } from 'react';
+import { Edit, Header } from './components';
+function App() {
+   const [isEdit, setEdit] = useState(false);
+   const [data, setData] = useState()
+   return (
+      <div className='App'>
+         
+      </div>
+   );
+}
+
+export default App;
---
+++ src/api.js
@@ -0,0 +1,1 @@
+export const api = 'https://course-api.com/react-tours-project'
